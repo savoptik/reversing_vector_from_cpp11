41f189d86124a7e05c987048872e8a67fa77c257
savoptik <sav70792@gmail.com>
2019-03-18 17:54:58 +0300

    Initial Commit

---
+++ reversing_vector_from_cpp11/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  reversing_vector_from_cpp11
+//
+//  Created by Артём Семёнов on 18/03/2019.
+//  Copyright © 2019 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
